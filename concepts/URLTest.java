98f4844b71f573e0558c69599b9fdc18a823b800
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-01-31 12:40:49 +0530

    Create URLTest.java

---
+++ concepts/URLTest.java
@@ -0,0 +1,35 @@
+package concepts;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLTest {
+
+    public static void main(String[] args) {
+
+        /** Output:
+         * URL http://google.com is valid
+         * URL http://index.html is valid
+         * java.net.MalformedURLException: no protocol: /index.html
+         * java.net.MalformedURLException: unknown protocol: t3
+         */
+
+        String[] urlStrings = {"http://google.com",
+                "/index.html",
+                "t3://hostname:7001",
+                "http://index.html"};
+        for (String urlString : urlStrings) {
+            try {
+                getIsURLValid(urlString);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void getIsURLValid(String urlString) throws MalformedURLException {
+
+        URL url = new URL(urlString);
+        System.out.println("URL " + url + " is valid");
+    }
+}
