3d6b703df744f0877301b3ea998d060c87265ced
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-01-31 12:28:06 +0530

    Create ListNode.java

---
+++ leetcodeproblems/pojo/ListNode.java
@@ -0,0 +1,20 @@
+package leetcodeproblems.pojo;
+
+public class ListNode {
+
+    //These fields are made public for the ease of use, from algorithms perspective
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
