1221149d382acf29c678ee49bd0ad00581f9eb8a
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-02-02 16:33:57 +0530

    Create ProductExceptSelf.java

---
+++ leetcodeproblems/ProductExceptSelf.java
@@ -0,0 +1,27 @@
+package leetcodeproblems;
+
+public class ProductExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+
+        int[] leftProduct = new int[nums.length];
+        int[] rightProduct = new int[nums.length];
+
+        leftProduct[0] = 1;
+        for(int i=1; i< nums.length; i++) {
+            leftProduct[i] = leftProduct[i-1] * nums[i-1];
+        }
+
+        rightProduct[nums.length-1] = 1;
+        for(int i=nums.length-2; i >=0; i--) {
+            rightProduct[i] = rightProduct[i+1] * nums[i+1];
+        }
+
+        int[] output = new int[nums.length];
+        for(int i=0; i< nums.length; i++) {
+            output[i] = leftProduct[i] * rightProduct[i];
+        }
+
+        return output;
+    }
+}
