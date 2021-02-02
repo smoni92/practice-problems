5358f55ca4815d0d0f6b699c46ac1b7af5af5cdd
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-02-02 16:33:55 +0530

    Create TreeNode.java

---
+++ leetcodeproblems/pojo/TreeNode.java
@@ -0,0 +1,31 @@
+package leetcodeproblems.pojo;
+
+public class TreeNode {
+
+    //These fields are made public for the ease of use, from algorithms perspective
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
