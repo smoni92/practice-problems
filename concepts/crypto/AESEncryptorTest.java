a0278f8f4e3476b7906c14c9e42340232dc0bad6
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-02-02 16:45:24 +0530

    Create AESEncryptorTest.java

---
+++ concepts/crypto/AESEncryptorTest.java
@@ -0,0 +1,65 @@
+package concepts.crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class AESEncryptorTest {
+
+    public static SecretKey stringToSecretKey(String encodedKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+        SecretKey originalKey = new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+        return originalKey;
+    }
+
+    public static String encrypt(String pText, SecretKey secretKey) throws Exception {
+
+        String encryptedString = null;
+        try {
+            SecretKeySpec skeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+            encryptedString = Base64.getEncoder().encodeToString(cipher.doFinal(pText.getBytes()));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+        return encryptedString;
+    }
+
+    public static String decrypt(String cText, SecretKey secretKey) throws Exception {
+
+        String encryptedString = null;
+        try {
+            SecretKeySpec skeySpec = new SecretKeySpec(secretKey.getEncoded(), "AES");
+            byte[] encryptText = Base64.getDecoder().decode(cText);
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+            encryptedString = new String(cipher.doFinal(encryptText));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+        return encryptedString;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String plainText = "originalString1";
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256);
+        SecretKey secretKey = keyGen.generateKey();
+
+        String encryptedText = encrypt(plainText, secretKey);
+        System.out.println(encryptedText);
+        System.out.println(decrypt(encryptedText, secretKey));
+
+        String keyString = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        encryptedText = encrypt(plainText, stringToSecretKey(keyString));
+        System.out.println(encryptedText);
+        System.out.println(decrypt(encryptedText, stringToSecretKey(keyString)));
+    }
+}
