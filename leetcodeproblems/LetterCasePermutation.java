4a81f0002cccb4da8bb83373b0df5993a1d9aab4
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-02-17 09:19:22 +0530

    Create LetterCasePermutation.java

---
+++ leetcodeproblems/LetterCasePermutation.java
@@ -0,0 +1,41 @@
+package leetcodeproblems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LetterCasePermutation {
+
+    public static List<String> letterCasePermutation(String S) {
+        if(S == null) {
+            return new ArrayList<>();
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(S);
+
+        for(int i = 0; i < S.length(); i++) {
+            if(Character.isDigit(S.charAt(i)))
+                continue;
+
+            int size = queue.size();
+            for(int j = 0; j < size; j++) {
+                String currString = queue.poll();
+                char[] currArr = currString.toCharArray();
+
+                currArr[i] = Character.toUpperCase(currArr[i]);
+                queue.offer(String.valueOf(currArr));
+
+                currArr[i] = Character.toLowerCase(currArr[i]);
+                queue.offer(String.valueOf(currArr));
+            }
+        }
+
+        return new LinkedList<>(queue);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(letterCasePermutation("a1B1"));
+    }
+}
