0e59884dadba0e9ab9137b3c7d3eb230a73fef7f
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-02-17 14:58:08 +0530

    Create ContainerWithMostWater.java

---
+++ leetcodeproblems/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+package leetcodeproblems;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        for(int i = 0, j = height.length - 1; i < j;) {
+            int currArea = ((j - i) * (Math.min(height[i], height[j])));
+            if(currArea > maxArea) {
+                maxArea = currArea;
+            }
+
+            if(height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return maxArea;
+    }
+}
