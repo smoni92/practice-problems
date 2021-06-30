db064b8ba05233aa24f5bddb902ea13050e73daa
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-06-30 22:31:02 +0530

    Create DefangIPAddress.java

---
+++ leetcode/DefangIPAddress.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class DefangIPAddress {
+
+    public static void main(String[] args) {
+
+        String IPAddress = "1.1.1.1";
+        //System.out.println(IPAddress.replaceAll(".","[.]"));
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i< IPAddress.length(); i++) {
+            if(IPAddress.charAt(i) == '.') {
+                sb.append("[.]");
+            } else {
+                sb.append(IPAddress.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
