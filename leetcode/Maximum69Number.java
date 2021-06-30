7d4b7a13704a796f5cb158671e7f10cd39aa8001
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-06-30 22:31:49 +0530

    Create Maximum69Number.java

---
+++ leetcode/Maximum69Number.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class Maximum69Number {
+
+    public static void main(String[] args) {
+        int result = maximum69Number(6699);
+        System.out.println("Result :" + result);
+    }
+
+    public static int maximum69Number(int num) {
+
+        return Integer.valueOf(String.valueOf(num).replaceFirst("6","9"));
+    }
+}
