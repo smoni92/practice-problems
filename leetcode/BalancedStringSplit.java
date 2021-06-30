5d768032e1069fc417400b2f565624dc8e0a87b8
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-06-30 22:30:48 +0530

    Create BalancedStringSplit.java

---
+++ leetcode/BalancedStringSplit.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class BalancedStringSplit {
+
+    public static void main(String[] args) {
+
+        int result = balancedStringSplit("RLLLLRRRLR");
+        System.out.println("Result : " + result);
+    }
+
+    public static int balancedStringSplit(String s) {
+        int result = 0;
+
+        Stack<Character> stack = new Stack();
+
+        for (Character ch : s.toCharArray()) {
+            if (stack.isEmpty() || stack.peek() == ch)
+                stack.push(ch);
+
+            if (!stack.isEmpty() && stack.peek() != ch)
+                stack.pop();
+
+            if (stack.isEmpty())
+                result++;
+        }
+
+        return result;
+    }
+}
