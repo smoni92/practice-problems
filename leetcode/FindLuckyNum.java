a8237c3a7ea9c1d2d90f69548a3be083bcedeaf9
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-01 20:15:50 +0530

    Update FindLuckyNum.java

--- leetcode/FindLuckyNum.java
+++ leetcode/FindLuckyNum.java
@@ -8,18 +8,18 @@ public class FindLuckyNum {
     public static int findLucky(int[] arr) {
 
         Map<Integer, Integer> arrMap = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (arrMap.containsKey(arr[i])) {
-                int count = arrMap.get(arr[i]);
-                arrMap.put(arr[i], count + 1);
+        for (int val : arr) {
+            if (arrMap.containsKey(val)) {
+                int count = arrMap.get(val);
+                arrMap.put(val, count + 1);
             } else {
-                arrMap.put(arr[i], 1);
+                arrMap.put(val, 1);
             }
         }
 
         int luckyNum = -1;
         for (Map.Entry<Integer, Integer> entry : arrMap.entrySet()) {
-            if (entry.getKey() == entry.getValue() && luckyNum < entry.getKey()) {
+            if (entry.getKey().equals(entry.getValue()) && luckyNum < entry.getKey()) {
                 luckyNum = entry.getKey();
             }
         }
@@ -28,7 +28,7 @@ public class FindLuckyNum {
     }
 
     public static void main(String[] args) {
-        int arr[] = {2, 2, 3, 4};
+        int[] arr = {2, 2, 3, 4};
         System.out.println(findLucky(arr));
     }
 }
