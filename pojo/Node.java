2e77f29d63cea495e1cafb46e450c94dbba46bc2
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-01 09:45:50 +0530

    Update Node.java

--- pojo/Node.java
+++ pojo/Node.java
@@ -10,6 +10,11 @@ public class Node {
         this.next = null;
     }
 
+    public Node(double data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
     @Override
     public String toString() {
         return "Node:: Data " + data;
