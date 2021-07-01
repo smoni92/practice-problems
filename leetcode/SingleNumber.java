12c6ea3f5cc3c41e3c48b38a4306c38cc403bf11
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-01 20:16:26 +0530

    Update SingleNumber.java

--- leetcode/SingleNumber.java
+++ leetcode/SingleNumber.java
@@ -27,8 +27,8 @@ public class SingleNumber {
 
     public static int singleNumberImproved(int[] nums) {
         int result = 0;
-        for (int i = 0; i < nums.length; i++) {
-            result ^= nums[i];
+        for (int num : nums) {
+            result ^= num;
         }
         return result;
     }
