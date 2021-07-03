072a0960686266f4bdd17bdaf889ca6e259ebf19
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-03 16:54:29 +0530

    Create KClosestElements.java

---
+++ leetcode/KClosestElements.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class KClosestElements {
+
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+
+        // Convert from array to list first to make use of Collections.sort()
+        List<Integer> sortedArr = new ArrayList<Integer>();
+        for (int num : arr) {
+            sortedArr.add(num);
+        }
+
+        // Sort using custom comparator
+        sortedArr.sort(Comparator.comparingInt(num -> Math.abs(num - x)));
+
+        // Only take k elements
+        sortedArr = sortedArr.subList(0, k);
+
+        // Sort again to have output in ascending order
+        sortedArr.sort(null);
+        return sortedArr;
+    }
+}
