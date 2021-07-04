5c9e9687708955af6c5b6121840716b880ee0617
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-04 21:05:25 +0530

    Create ClimbStairs.java

---
+++ leetcode/ClimbStairs.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class ClimbStairs {
+
+    public static int climbStairs(int n) {
+        if (n == 1) {
+            return n;
+        }
+
+        int[] steps = new int[n + 1];
+        steps[1] = 1;
+        steps[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            steps[i] = steps[i - 1] + steps[i - 2];
+        }
+
+        return steps[n];
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+    }
+}
