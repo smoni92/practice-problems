aa8a4287f8d4dcad041837d3dceaca599debd68a
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-13 19:21:41 +0530

    Create LengthOfLongestSubstring.java

---
+++ leetcode/LengthOfLongestSubstring.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthOfLongestSubstring {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        int max = 0;
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (int rightPointer = 0, leftPointer = 0; rightPointer < s.length(); rightPointer++) {
+            while (charMap.containsKey(s.charAt(rightPointer))) {
+                charMap.remove(s.charAt(leftPointer));
+                leftPointer++;
+            }
+            charMap.put(s.charAt(rightPointer), rightPointer);
+            max = Math.max(max, rightPointer - leftPointer + 1);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("abcabcba"));
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("aab"));
+        System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("dvdf"));
+    }
+}
