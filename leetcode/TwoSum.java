9dd7d90dbb87b6e8bb44e6fa964fd17c1c1be62f
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-20 12:00:47 +0530

    Update TwoSum.java

--- leetcode/TwoSum.java
+++ leetcode/TwoSum.java
@@ -9,13 +9,13 @@ public class TwoSum {
 
         int[] indices = new int[2];
         Map<Integer, Integer> numsMap = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             numsMap.put(nums[i], i);
         }
 
-        for(int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
-            if(numsMap.containsKey(diff) && numsMap.get(diff) != i) {
+            if (numsMap.containsKey(diff) && numsMap.get(diff) != i) {
                 indices[0] = i;
                 indices[1] = numsMap.get(diff);
                 break;
@@ -26,10 +26,14 @@ public class TwoSum {
 
     public static void main(String[] args) {
 
-        int[] nums = {2,7,11,15};
+        int[] nums = {2, 7, 11, 15};
         int target = 9;
 
+        System.out.println("Two Sum...");
+        long start = System.nanoTime();
         int[] result = twoSum(nums, target);
-        System.out.println(result[0] + " " + result[1]);
+        long timeTaken = System.nanoTime() - start;
+        System.out.println("Indices: " + result[0] + " " + result[1] + " Time Taken : " + timeTaken + " ns");
+
     }
 }
