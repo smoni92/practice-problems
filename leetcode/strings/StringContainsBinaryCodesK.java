1506dab343af62f50428ec56bd22a3143b0518e2
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-21 20:39:45 +0530

    Create StringContainsBinaryCodesK.java

---
+++ leetcode/strings/StringContainsBinaryCodesK.java
@@ -0,0 +1,22 @@
+package leetcode.strings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StringContainsBinaryCodesK {
+
+    public boolean hasAllCodes(String s, int k) {
+
+        Set<String> binaryCodes = new HashSet<>();
+        for(int i = 0 ; i < s.length() - k + 1; i++) {
+            String sub = s.substring(i, i+k);
+            if(!binaryCodes.contains(sub)) {
+                binaryCodes.add(sub);
+                if(binaryCodes.size() == Math.pow(2, k)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
