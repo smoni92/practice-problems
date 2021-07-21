bf3e4fcc55b5fac1e783947353f56999daecaec1
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-21 20:43:36 +0530

    Create LongestCommonPrefixStrings.java

---
+++ leetcode/strings/LongestCommonPrefixStrings.java
@@ -0,0 +1,41 @@
+package leetcode.strings;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefixStrings {
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length < 1) {
+            return "";
+        }
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        Arrays.sort(strs);
+
+        String first = strs[0].length() < strs[strs.length - 1].length() ? strs[0] : strs[strs.length - 1];
+        String second = strs[0].length() < strs[strs.length - 1].length() ? strs[strs.length - 1] : strs[0];
+
+        int min = Math.min(first.length(), second.length());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < min; i++) {
+            if (first.charAt(i) == second.charAt(i)) {
+                sb.append(first.charAt(i));
+            } else {
+                break;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println("Result : " + longestCommonPrefix(strs));
+
+        strs = new String[]{"reflower", "flow", "flower"};
+        System.out.println("Result : " + longestCommonPrefix(strs));
+    }
+
+}
