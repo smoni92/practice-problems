11fbca255c607572c5ed51e3cda864fe5eaeb046
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-21 20:43:17 +0530

    Create DistributeCandies.java

---
+++ leetcode/misc/DistributeCandies.java
@@ -0,0 +1,15 @@
+package leetcode.misc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class DistributeCandies {
+
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> uniqueCandies = new HashSet<>();
+        for(int candy : candyType) {
+            uniqueCandies.add(candy);
+        }
+        return Math.min(uniqueCandies.size(), candyType.length/2);
+    }
+}
