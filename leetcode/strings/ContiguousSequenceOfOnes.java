2f5ae54a67e7cbdfff91693f609b51beb029642d
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-21 20:39:37 +0530

    Create ContiguousSequenceOfOnes.java

---
+++ leetcode/strings/ContiguousSequenceOfOnes.java
@@ -0,0 +1,32 @@
+package leetcode.strings;
+
+public class ContiguousSequenceOfOnes {
+
+    public boolean checkOnesSegment(String s) {
+        int longestOnes = 0;
+        int currentOnes = 0;
+        boolean foundOne = false;
+
+        char[] sArr = s.toCharArray();
+        for (char ch : sArr) {
+            if (ch == '1') {
+                if (foundOne) {
+                    return false;
+                }
+                longestOnes = Math.max(longestOnes, ++currentOnes);
+            } else {
+                if (longestOnes > 0) {
+                    foundOne = true;
+                }
+                currentOnes = 0;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ContiguousSequenceOfOnes contiguousSequenceOfOnes = new ContiguousSequenceOfOnes();
+        System.out.println(contiguousSequenceOfOnes.checkOnesSegment("1001"));
+        System.out.println(contiguousSequenceOfOnes.checkOnesSegment("110"));
+    }
+}
