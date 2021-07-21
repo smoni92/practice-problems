53f50558bcbde2af1b77b0021d70956f5efddbe5
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-07-21 20:45:44 +0530

    Create LinkedListCycle.java

---
+++ leetcode/listnode/LinkedListCycle.java
@@ -0,0 +1,36 @@
+package leetcode.listnode;
+
+import pojo.ListNode;
+
+public class LinkedListCycle {
+
+    public static boolean hasCycle(ListNode head) {
+        if(head == null) {
+            return false;
+        }
+
+        ListNode slowPtr = head, fastPtr = head;
+        while(slowPtr.next != null && fastPtr.next != null && fastPtr.next.next != null) {
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+            if(slowPtr == fastPtr) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode l2 = new ListNode(2, null);
+        ListNode l1 = new ListNode(1, l2);
+
+        System.out.println(hasCycle(l1));
+
+        ListNode l31 = new ListNode(3);
+        ListNode l21 = new ListNode(2, l31);
+        ListNode l11 = new ListNode(1, l21);
+        l31.next = l11;
+
+        System.out.println(hasCycle(l11));
+    }
+}
