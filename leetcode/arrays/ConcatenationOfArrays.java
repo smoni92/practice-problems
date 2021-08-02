738ad46e306b5795191a780f68810eaf6dc8b6c0
Monica Sankar <34513207+smoni92@users.noreply.github.com>
2021-08-02 20:50:57 +0530

    Create ConcatenationOfArrays.java

---
+++ leetcode/arrays/ConcatenationOfArrays.java
@@ -0,0 +1,14 @@
+package leetcode.arrays;
+
+public class ConcatenationOfArrays {
+
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n * 2];
+        for(int i = 0 ; i < n; i ++) {
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+}
